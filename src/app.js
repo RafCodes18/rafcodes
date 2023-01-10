2cb9a133de637e4f3071131e2bcc042b6d7b7686
Rafael Parra <700175134@fvtc.edu>
2023-01-10 14:31:10 -0600

    fixed sig animation

--- src/app.js
+++ src/app.js
@@ -8,3 +8,33 @@ function toggleButton(){
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+
+
+
+
+
+//animate on scroll
+let footer = document.getElementById("contact");
+var svg = document.getElementById("svg");
+let sig=document.getElementById('sig');
+
+window.addEventListener("scroll", function(){
+    if(isInViewport(footer)){
+        if(!svg.classList.contains('animate')){
+            svg.classList.add("animate");
+        }
+    }else{
+        svg.classList.remove("animate");
+    }
+});
+
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || html.clientHeight) &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+}
